1cb643b16e8d5ae36926f1bd84dcaa59058eb4b5
wenyi420 <t0987320406@gmail.com>
2022-07-03 20:45:31 +0800

    [FEAT] 將 movie 常用 icon 細分成組件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,8 @@ import { reactive } from "@vue/reactivity";
 
 import MovieModal from "@/components/Global/Modal/movieModal.vue";
 
+import PlayIcon from "@/components/Icon/play.vue";
+
 const store = useUserStore();
 const { isLogined } = storeToRefs(store);
 
@@ -105,19 +107,7 @@ const getSimilarMovies = (genres) => {
           <div class="movie-btn play">
             <div>
               <span class="movie-btn-icon">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="Hawkins-Icon Hawkins-Icon-Standard"
-                >
-                  <path
-                    d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
+                <PlayIcon />
               </span>
               <span class="movie-btn-text">播放</span>
             </div>
